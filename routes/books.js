9e94e1decc658d32631a9ba2eacd27319b70ecbb
JohnG585 <jgallagher585@gmail.com>
2017-03-27 18:53:36 -0600

    books/pt1 complete

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,11 @@
 'use strict';
-
+const path = require('path');
 const express = require('express');
-
 // eslint-disable-next-line new-cap
 const router = express.Router();
-
+router.get('/', function(req, res, next) {
+  res.render('');
+});
 // YOUR CODE HERE
 
 module.exports = router;
