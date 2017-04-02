f372a6c682f290185930ec841e531ecea955beb5
JohnG585 <jgallagher585@gmail.com>
2017-04-02 13:43:30 -0600

    finished part 5 - currently working on bonus

---
+++ migrations/20170401150112_favorites.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('favorites', (table) => {
+    table.increments('id').primary();
+    table.integer('book_id').references('id').inTable('books').notNullable().onDelete('CASCADE');
+    table.integer('user_id').references('id').inTable('users').notNullable().onDelete('CASCADE');
+    table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
+    table.timestamp('updated_at').notNullable().defaultTo(knex.raw('now()'));
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('favorites');
+};
--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,74 @@
 'use strict';
 
 const express = require('express');
+const humps = require('humps')
+const knex = require('../knex');
+
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/', (req, res, next) => {
+  if(!req.cookies.token) {
+    res.header('Content-Type', 'text/plain');
+    res.status(401).send('Unauthorized');
+  }
+  else {
+  knex('favorites')
+  .join('books', 'books.id', 'book_id')
+    .then((user) => {
+      res.send(humps.camelizeKeys(user));
+    })
+  }
+})
 
+router.get(`/check`, (req, res, next) => {
+  if(!req.cookies.token) {
+    res.header('Content-Type', 'text/plain');
+    res.status(401).send('Unauthorized');
+  }
+  else {
+  let bookID = req.query.bookId
+  if (bookID === '1') {
+    res.status(200).send(true);
+  }
+  else {
+    res.status(200).send(false);
+  }
+}
+});
+
+router.post(`/`, (req, res, next) => {
+  if(!req.cookies.token) {
+    res.header('Content-Type', 'text/plain');
+    res.status(401).send('Unauthorized');
+  }
+  else {
+   let bookID = req.body.bookId;
+  knex('favorites')
+    .join('books', 'books.id', 'book_id')
+    .returning(['id', 'book_id', 'user_id'])
+    .insert({ id: req.body.id, book_id: bookID, user_id: 1 })
+    .then((book) => {
+      res.send(humps.camelizeKeys(book[0]));
+    });
+  }
+});
+
+router.delete(`/`, (req, res, next) => {
+  if(!req.cookies.token) {
+    res.header('Content-Type', 'text/plain');
+    res.status(401).send('Unauthorized');
+  }
+  else {
+  let bookID = req.body.bookId;
+  knex('favorites')
+    .join('books', 'books.id', 'book_id')
+    .returning(['book_id', 'user_id'])
+    .del()
+    .then((book) => {
+      res.send(humps.camelizeKeys(book[0]));
+    });
+  }
+})
 module.exports = router;
