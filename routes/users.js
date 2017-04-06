26fdfbcc1b5113f2b422a0ca41e07fe3ef99d7b6
JohnG585 <jgallagher585@gmail.com>
2017-04-06 14:54:49 -0600

    changed hash

--- routes/users.js
+++ routes/users.js
@@ -12,14 +12,15 @@ const validations = require('../validations/users');
 const router = express.Router();
 
 router.post('/', ev(validations.post), (req, res, next) => {
-  bcrypt.hash(req.body.password, 12)
+  const salt = bcrypt.genSaltSync()
+  const hash = bcrypt.hashSync(req.body.password, salt)
     .then((hashed_password) => {
       return knex('users')
         .insert({
           first_name: req.body.firstName,
           last_name: req.body.lastName,
           email: req.body.email,
-          hashed_password: hashed_password
+          hashed_password: hash
         }, '*');
     })
     .then((users) => {
